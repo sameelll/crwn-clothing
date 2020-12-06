4c3be2746e8218b3661514f1cd0c8b630b1bedb9
Eren <eren.akel.46@gmail.com>
2020-12-06 16:54:38 +0300

    finished styling

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import React from 'react';
+import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 
-import './App.css';
-
-class App extends React.Component {
-  render() {
-    return (
-      <div className='App'>
-        <HomePage />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
 }
 
 export default App;
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
 
-const MenuItem = ({ title, imageUrl, size }) => (
+import './menu-item.styles.scss';
 
-    <div style={{
-        backgroundImage: `url${imageUrl}`
-    }} className= {`${ size } menu-item`}>
-    
-        <div className='content'>
-          <div className='title'>{ title }</div>
-          <span className='subtitle'>SHOP NOW</span>
-        </div>
+const MenuItem = ({ title, imageUrl, size }) => (
+  <div className={`${size} menu-item`}>
+    <div
+      className='background-image'
+      style={{
+        backgroundImage: `url(${imageUrl})`
+      }}
+    />
+    <div className='content'>
+      <h1 className='title'>{title.toUpperCase()}</h1>
+      <span className='subtitle'>SHOP NOW</span>
     </div>
+  </div>
 );
 
 export default MenuItem;
